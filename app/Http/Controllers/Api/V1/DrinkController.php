92925abf5f48eee558905cf382e0941de2423e10
Nuno Correia <nuno_correia_96@hotmail.com>
2025-05-03 17:22:56 +0200

    Add DrinkController and API v1 routing
    Introduce DrinkController with basic resource methods. Configure a new API v1 route group and register Drink resource routes in `api_v1.php`. Update `RouteServiceProvider` to include API versioning structure.

---
+++ app/Http/Controllers/Api/V1/DrinkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Drink;
+use Illuminate\Http\Request;
+
+class DrinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Drink::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Drink $drink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Drink $drink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Drink $drink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Drink $drink)
+    {
+        //
+    }
+}
---
+++ routes/api_v1.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\V1\DrinkController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Now create something great!
+|
+*/
+
+Route::apiResource('drinks', DrinkController::class);
+
+
