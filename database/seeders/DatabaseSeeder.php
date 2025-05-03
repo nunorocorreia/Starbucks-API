ab1827784b137f7e933ad154e0aa500aaa15f9ee
Nuno Correia <nuno_correia_96@hotmail.com>
2025-05-03 16:34:57 +0200

    Add Drink, Category, and Extras models with migrations
    Created models for Drink, Category, and Extras along with their respective migrations, factories, and seed data. Updated the database seeder to include sample data for drinks, categories, and extras.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Drink;
+use App\Models\Extras;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +17,98 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Drink::factory()->createMany([
+            [
+                'name' => 'Latte',
+                'price' => 3.50,
+                'stock' => 10,
+                'id_category' => 1,
+            ],
+            [
+                'name' => 'Mocha',
+                'price' => 2.50,
+                'stock' => 20,
+                'id_category' => 1,
+            ],
+            [
+                'name' => 'Mocchiato',
+                'price' => 4.00,
+                'stock' => 15,
+                'id_category' => 1,
+            ],
+            [
+                'name' => 'Cappuccino',
+                'price' => 2.00,
+                'stock' => 30,
+                'id_category' => 1,
+            ],
+            [
+                'name' => 'Americano',
+                'price' => 1.50,
+                'stock' => 25,
+                'id_category' => 1,
+            ],
+            [
+                'name' => 'Filter Coffee',
+                'price' => 3.00,
+                'stock' => 12,
+                'id_category' => 2,
+            ],
+            [
+                'name' => 'Coffe Misto',
+                'price' => 1.75,
+                'stock' => 40,
+                'id_category' => 2,
+            ],
+            [
+                'name' => 'Mint',
+                'price' => 2.25,
+                'stock' => 18,
+                'id_category' => 3,
+            ],
+            [
+                'name' => 'Chamomile Herbal',
+                'price' => 3.75,
+                'stock' => 22,
+                'id_category' => 3,
+            ],
+            [
+                'name' => 'Earl Grey',
+                'price' => 4.50,
+                'stock' => 8,
+                'id_category' => 3,
+            ]
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Category::factory()->createMany([
+            [
+                'name' => 'Espresso Drinks',
+            ],
+            [
+                'name' => 'Brewed Coffee',
+            ],
+            [
+                'name' => 'Tea',
+            ]
+        ]);
+
+        Extras::factory()->createMany([
+            [
+                'name' => 'Cinnamon',
+                'price' => 0.45,
+            ],
+            [
+                'name' => 'Yellow Sugar',
+                'price' => 0.60,
+            ],
+            [
+                'name' => 'Syrup',
+                'price' => 0.50,
+            ],
+            [
+                'name' => 'Whipped Cream',
+                'price' => 0.25,
+            ]
         ]);
     }
 }
